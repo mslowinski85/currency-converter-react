7c8dbcfc148396591701d60aadf24ef14580f360
Marek Slowinski <marekslowinski@gmail.com>
2023-02-02 20:12:06 +0100

    onChange function to inputSum added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Form from "./Form";
 import Container from "./Container";
 
+
+
 function App() {
+  const [sum, setSum] = useState("");
   return (
     <Container>
       <Header />
-      <Form />
+      <Form setSum={setSum}/>
       <Footer />
     </Container>
   );
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,67 +1,72 @@
+import { useState } from "react";
 import List from "../List";
 import "./style.css";
 
-const Form = () => (
-  <form className="form">
-    <fieldset>
-      <p>
+const Form = () => {
+  const [sum, setSum] = useState("");
+  return (
+    <form className="form">
+      <fieldset>
+        <p>
+          <label>
+            <span className="form__labelTextRequired">Pole wymagane*</span>
+          </label>
+        </p>
+        <p>
+          <label>
+            <span className="form__labelText">Wpisz kwotę*: </span>
+            <input
+              className="form__input"
+              value={sum}
+              onChange={(event => setSum(event.target.value))}
+              name="inputSum"
+              placeholder="Minimalna kwota to 0.01!"
+              type="number"
+              min="0.01"
+              step="0.01"
+              autofocus
+              required
+            />
+          </label>
+        </p>
         <label>
-          <span className="form__labelText">Pole wymagane*</span>
+          <span className="form__labelText"> Wybierz walutę:</span>
         </label>
-      </p>
-      <p>
-        <label>
-          <span className="form__labelText">Wpisz kwotę*: </span>
-          <input
-            className="form__input"
-            value=""
-            name="inputSum"
-            placeholder="Minimalna kwota to 0.01!"
-            type="number"
-            min="0.01"
-            step="0.01"
-            autofocus
-            required
-          />
-        </label>
-      </p>
-      <label>
-        <span className="form__labelText"> Wybierz walutę:</span>
-      </label>
-      <List />
-      <p>
+        <List />
+        <p>
+          <label>
+            <span className="form__labelText">
+             Automatyczne przeliczenie na:
+            </span>
+            <input
+              className="form__input"
+              name="pln"
+              value="PLN - Polski złoty"
+              disabled
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <span className="form__labelText">
+              Aktualny kurs wybranej waluty:
+            </span>
+          </label>
+          <input value={currentRate} className="form__input" name="currentRate" readonly />
+        </p>
         <label>
           <span className="form__labelText">
-            Kalkulator automatycznie przeliczy na:
+            <strong className="form__sumLabel">Kwota po przeliczeniu:</strong>
           </span>
           <input
-            className="form__input"
-            name="pln"
-            value="PLN - Polski złoty"
-            disabled
+            className="form__input form__input--finallySum"
+            name="endSum"
+            readonly
           />
         </label>
-      </p>
-      <p>
-        <label>
-          <span className="form__labelText">
-            Aktualny kurs wybranej waluty:
-          </span>
-        </label>
-        <input className="form__input" name="currentRate" readonly />
-      </p>
-      <label>
-        <span className="form__labelText">
-          <strong className="form__sumLabel">Kwota po przeliczeniu:</strong>
-        </span>
-        <input
-          className="form__input form__input--finallySum"
-          name="endSum"
-          readonly
-        />
-      </label>
-    </fieldset>
-  </form>
-);
+      </fieldset>
+    </form>
+  );
+};
 
 export default Form;
